39d9bf78504775b3611d5cb93ddea75d7ae552f0
AV-room <AVroom.github@gmail.com>
2018-07-24 10:19:26 +1000

    first commit

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PhotoGrid extends React.Component {
+  render() {
+    return (
+      <div className='photo-grid'>
+        PHOTO-GRIDDY-GRID-GRID
+      </div>
+    )
+  }
+}
+
+export default PhotoGrid;
