5585751f87d677d0f9a708b0ff5047023a8dfb5a
AV-room <AVroom.github@gmail.com>
2018-07-25 14:33:52 +1000

    inject store data and actions into Main component

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,6 +3,7 @@
 // (2) a COPY of the current state
 
 function posts(state = [], action) {
+  console.log('the post will change');
   console.log(state, action);
   return state;
 }
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -3,10 +3,11 @@ import { render } from 'react-dom';
 
 // replaced browserHistory with history exported from store.js
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
 import { Provider } from 'react-redux';
 import { store, history } from './store';
-import Main from './components/Main';
+
+// import Main from './components/Main';
+import App from './components/App';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 import css from './styles/style.styl';
@@ -14,7 +15,7 @@ import css from './styles/style.styl';
 const router = (
   <Provider store={ store }>
     <Router history={ history }>
-      <Route path="/" component={ Main }>
+      <Route path="/" component={ App }>
         <IndexRoute component={ PhotoGrid }></IndexRoute>
         <Route path="/view/:postId" component={ Single }></Route>
       </Route>
