42468d9fe0d0f923afb4dc69189399992c86898e
AV-room <AVroom.github@gmail.com>
2018-07-26 11:25:33 +1000

    fleshed out photo component

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Photo from './Photo';
 
 class PhotoGrid extends React.Component {
   render() {
     return (
       <div className='photo-grid'>
-        PHOTO-GRIDDY-GRID-GRID
+        {
+          this.props.posts.map((post, i) =>
+            <Photo {...this.props} key={ i } i={ i } post={ post} />
+          )
+        }
       </div>
     )
   }
